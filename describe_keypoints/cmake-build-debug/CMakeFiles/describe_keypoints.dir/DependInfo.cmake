37012c0b5ad3d93679c9d1eafc7daa0fa940bb9a
Cedric-Perauer <Ceddyperauer@gmail.com>
2019-12-13 22:56:13 +0100

    Lesson 4 Finished

---
+++ describe_keypoints/cmake-build-debug/CMakeFiles/describe_keypoints.dir/DependInfo.cmake
@@ -0,0 +1,71 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cedric/SFND_Camera/describe_keypoints/src/describe_keypoints.cpp" "/home/cedric/SFND_Camera/describe_keypoints/cmake-build-debug/CMakeFiles/describe_keypoints.dir/src/describe_keypoints.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/cedric/Downloads/opencv/build"
+  "/home/cedric/Downloads/opencv/include"
+  "/home/cedric/Downloads/opencv/modules/core/include"
+  "/home/cedric/Downloads/opencv/modules/flann/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/hdf/include"
+  "/home/cedric/Downloads/opencv/modules/imgproc/include"
+  "/home/cedric/Downloads/opencv/modules/ml/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/phase_unwrapping/include"
+  "/home/cedric/Downloads/opencv/modules/photo/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/plot/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/reg/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/surface_matching/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/viz/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/xphoto/include"
+  "/home/cedric/Downloads/opencv/modules/dnn/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/freetype/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/fuzzy/include"
+  "/home/cedric/Downloads/opencv/modules/gapi/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/hfs/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/img_hash/include"
+  "/home/cedric/Downloads/opencv/modules/imgcodecs/include"
+  "/home/cedric/Downloads/opencv/modules/videoio/include"
+  "/home/cedric/Downloads/opencv/modules/highgui/include"
+  "/home/cedric/Downloads/opencv/modules/ts/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/bioinspired/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/dnn_objdetect/include"
+  "/home/cedric/Downloads/opencv/modules/features2d/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/line_descriptor/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/saliency/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/text/include"
+  "/home/cedric/Downloads/opencv/modules/calib3d/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/ccalib/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/datasets/include"
+  "/home/cedric/Downloads/opencv/modules/objdetect/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/rgbd/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/shape/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/stereo/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/structured_light/include"
+  "/home/cedric/Downloads/opencv/modules/video/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/videostab/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/xfeatures2d/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/ximgproc/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/xobjdetect/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/aruco/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/bgsegm/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/dpm/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/face/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/optflow/include"
+  "/home/cedric/Downloads/opencv/modules/stitching/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/superres/include"
+  "/home/cedric/Downloads/opencv_contrib/modules/tracking/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ descriptor_matching/cmake-build-debug/CMakeFiles/descriptor_matching.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/descriptor_matching.dir/src/descriptor_matching.cpp.o"
+  "CMakeFiles/descriptor_matching.dir/src/structIO.cpp.o"
+  "descriptor_matching"
+  "descriptor_matching.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/descriptor_matching.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
