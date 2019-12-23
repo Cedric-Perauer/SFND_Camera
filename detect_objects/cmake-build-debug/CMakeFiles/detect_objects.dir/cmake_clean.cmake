058ed3c52896f5524d160f317a517eff44adab87
Cedric Perauer <40869163+Cedric-Perauer@users.noreply.github.com>
2019-12-23 12:58:57 +0100

    exercises done

---
+++ detect_objects/cmake-build-debug/CMakeFiles/detect_objects.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/detect_objects.dir/src/detect_objects_2.cpp.o"
+  "detect_objects"
+  "detect_objects.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/detect_objects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
